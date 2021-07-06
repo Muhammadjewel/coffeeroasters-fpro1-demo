da8970a09d65299c7f2089debc3cd0bd6058ba0b
muhammadjewel <msuratov@gmail.com>
2021-07-06 18:21:44 +0500

    Add new-plan section (mobile)

--- js/main.js
+++ js/main.js
@@ -1,12 +1,26 @@
 var modifiers = {
-  siteHeaderOpen: 'site-header--open'
+  siteHeaderOpen: 'site-header--open',
+  formGroupOpen: 'form-group--open'
 };
 
+// SITE-HEADER
 var elSiteHeader = document.querySelector('.site-header');
 var elSiteHeaderToggler = elSiteHeader.querySelector('.js-site-header-toggler');
 
 if (elSiteHeaderToggler) {
   elSiteHeaderToggler.addEventListener('click', function () {
     elSiteHeader.classList.toggle(modifiers.siteHeaderOpen);
+  });
+}
+
+
+// FORM-GROUP
+var elsFormGroupToggler = document.querySelectorAll('.js-form-group-toggler');
+
+if (elsFormGroupToggler.length > 0) {
+  elsFormGroupToggler.forEach(function (toggler) {
+    toggler.addEventListener('click', function () {
+      toggler.closest('.form-group').classList.toggle(modifiers.formGroupOpen);
+    });
   });
 }
